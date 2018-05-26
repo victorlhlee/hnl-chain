b339aa960561e02c82f40d988a174ce392e54709
Victor <viclee02@gmail.com>
2018-05-25 23:17:52 -1000

    adds 10 blocks

--- dev-test.js
+++ dev-test.js
@@ -1,8 +1,15 @@
-const Block = require('./block');
+// const Block = require('./block');
 
-// const block = new Block('foo', 'bar', 'zoo', 'baz');
-// console.log(block.toString());
-// console.log(Block.genesis().toString());
+// // const block = new Block('foo', 'bar', 'zoo', 'baz');
+// // console.log(block.toString());
+// // console.log(Block.genesis().toString());
 
-const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
-console.log(fooBlock.toString());
+// const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
+// console.log(fooBlock.toString());
+
+const Blockchain = require('./blockchain');
+const bc = new Blockchain();
+
+for(let i = 0; i<10; i++){
+    console.log(bc.addBlock(`foo ${i}`).toString());
+}
